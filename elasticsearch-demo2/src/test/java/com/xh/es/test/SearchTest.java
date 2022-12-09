d624f4fdc9ed30aee46d472122995bc7aea58526
Andrew <deeplhub@users.noreply.github.com>
2022-12-09 09:12:24 +0800

    search

--- elasticsearch-demo2/src/test/java/com/xh/es/test/SearchTest.java
+++ elasticsearch-demo2/src/test/java/com/xh/es/test/SearchTest.java
@@ -1,8 +1,15 @@
 package com.xh.es.test;
 
+import com.xh.es.example.model.ProductEntity;
+import com.xh.es.example.repository.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -15,4 +22,37 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SearchTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    /**
+     * term 查询
+     * search(termQueryBuilder) 调用搜索方法，参数查询构建器对象
+     */
+    @Test
+    public void termQuery() {
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("title", "小米");
+        Iterable<ProductEntity> products = productRepository.search(termQueryBuilder);
+        for (ProductEntity product : products) {
+            System.out.println(product);
+        }
+    }
+
+    /**
+     * term 查询加分页
+     */
+    @Test
+    public void termQueryByPage() {
+        int currentPage = 0;
+        int pageSize = 5;
+        //设置查询分页
+        PageRequest pageRequest = PageRequest.of(currentPage, pageSize);
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("title", "小米");
+        Iterable<ProductEntity> products = productRepository.search(termQueryBuilder, pageRequest);
+        for (ProductEntity product : products) {
+            System.out.println(product);
+        }
+    }
+
 }
